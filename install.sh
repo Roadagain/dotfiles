6aac1129fb78dff29f9785176ac44ae436f4bb30
Roadagain <ringoh72@gmail.com>
2016-02-01 22:45:10 +0900

    Add installing NeoBundle to install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,10 @@
 
 echo Thank you for installing Roadagain\'s dotfiles!
 
+# vim
 echo Installing vimrc...
 cp vimrc ~/.vimrc
+echo Installing NeoBundle...
+sh neobundle.sh
 
 echo Installation was successfully completed!
