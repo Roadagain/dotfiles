a267b216b1db20864422f08570a5ce2cf234ce6d
Roadagain <ringoh72@gmail.com>
2016-02-01 22:52:45 +0900

    Add install script to install.sh

--- install.sh
+++ install.sh
@@ -5,4 +5,8 @@ echo Thank you for installing Roadagain\'s dotfiles!
 echo Installing vimrc...
 cp vimrc ~/.vimrc
 
+# gitconfig
+echo Installing gitconfig...
+cp gitconfig ~/.gitconfig
+
 echo Installation was successfully completed!
